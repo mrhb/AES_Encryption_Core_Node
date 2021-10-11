9c584decb9ce3a09605c5140cfa32fa3d7832188
M.Reza Hajjar43 <mmmhajjar83@gmail.com>
2021-10-11 12:37:56 +0330

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AES_Encryption_Core_Node
 Encrypt in node js and decrypt it in dotNet Core and vice versa
+these codes is testes in react native:<link>https://snack.expo.dev/@mrhbmrhb/first-</link>
--- javascript/index.mjs
+++ javascript/index.mjs
@@ -1,35 +1,14 @@
 import CryptoES from 'crypto-es';
+import { Encrypt,Decrypt } from "./AES_crypto_es.mjs";
 
+//import AES from './AES_crypto_es.ts';
+
+
+//**************Encryption************** */
 
 const passPhrase="salamMRezaHajjar";
-const cipherText="aiNKr9aehhBKbsc3mclkEJprvxFMSIK+xOwlUkKj7ovrTnLDeDjWc0OmQRBw7cRdVRLOyjOxHmcLbddawKhw9g==";
+const plainText="Plain text input to Nodejs ";
+console.log(Encrypt(plainText,passPhrase));
 
-
-
-
-
-const salt = CryptoES.enc.Utf8.parse('salamMRezaHajjar');
-const key = CryptoES.PBKDF2(passPhrase, salt, { keySize: 128/32, iterations: 1000 });
-const iv = CryptoES.enc.Utf8.parse('salamMRezaHajjar');
-
-//const base64_iv = CryptoES.enc.Base64.stringify(iv.words);
-
-const encrypted = CryptoES.AES.encrypt("Plain text input to Nodejs ",  key, { iv: iv }, { mode: CryptoES.mode.CBC, padding: CryptoES.pad.Pkcs7 });
-console.log(encrypted.iv.words);
-
-
-let arrs = [salt.words,encrypted.iv.words,encrypted.ciphertext.words];
-arrs=arrs.reduce((a, b) => [...a, ...b], []);
-//console.log(arrs);
-
-const words = CryptoES.lib.WordArray.create(arrs);
-const base64 = CryptoES.enc.Base64.stringify(words);
-
-
-console.log("encrypted as base264: "+base64);
-
-
-const iv_words =CryptoES.enc.Base64.parse(base64).words;
-const decrypted = CryptoES.AES.decrypt(encrypted,key, { iv: CryptoES.lib.WordArray.create(iv_words.slice(0,4)) });
-console.log("decrypted as latin: "+CryptoES.enc.Latin1.stringify(decrypted));
-
+//**************Decryption************** */
+console.log(Decrypt(Encrypt(plainText,passPhrase),passPhrase));
